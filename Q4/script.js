5878b96a11bccf8a7f30830cda883bdbc4e1145f
infinitum0007 <rohit2002sah@gamil.com>
2023-10-19 09:37:31 +0530

    dom changes

--- Q4/script.js
+++ Q4/script.js
@@ -9,10 +9,15 @@ window.onload = function () {
   };
   const getCardDiv = function (n, i, rgb, fontSize) {
     let div = document.createElement("div");
+    let nav = document.getElementById("nav");
+    let title = document.getElementById("heading");
     div.classList.add("card");
     div.innerText = getString(n, i);
     div.style.color = rgb;
     div.style.fontSize = `${fontSize}px`;
+    nav.style.boxShadow=`0px 2px 3px 0px ${rgb}`;
+    title.innerText=`Table of ${n}`
+    // title.style.color=rgb;
     // console.log(div);
     return div;
   };
@@ -32,7 +37,7 @@ window.onload = function () {
 
   setInterval(() => {
     // while (state.index < 11 || state.value != 20) {
-      if(state.index < 11 || state.value != 20){   //table control 
+      if(state.index < 11 || state.value != 10){   //table control 
         
           if (state.index > 10) {
               state.value += 1;
@@ -54,5 +59,5 @@ window.onload = function () {
             //   console.log(state);
             // }
     }
-  }, 1000);
+  }, 500);
 };
